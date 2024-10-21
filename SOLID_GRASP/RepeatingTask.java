60edee2f6020b1593623a79d74ffa76e514705af
Blake-Berman <blakeberman2005@gmail.com>
2024-10-20 21:41:02 -0400

    Modifying SOLID_GRASP assignment to ensure it compiles correctly

--- SOLID_GRASP/RepeatingTask.java
+++ SOLID_GRASP/RepeatingTask.java
@@ -2,7 +2,7 @@ public class RepeatingTask extends Task {
     private int daysInBetweenReocurrence;
     
     public RepeatingTask(String title, String description, String dueDate, String status, int priority, int frequency) {
-        super(title, description, dueDate, status, priority);
+        super(title, description, dueDate, priority);
         this.daysInBetweenReocurrence = frequency;
     }
 
