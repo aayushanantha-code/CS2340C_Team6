37211530aee900d194b6131bc238c7e680f75e26
EuanHam <euanham05@gmail.com>
2024-10-22 23:26:26 -0400

    Added users to destinations

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/model/Destination.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/model/Destination.java
@@ -1,15 +1,20 @@
 package com.example.sprintproject.model;
 
+import java.util.List;
+
 public class Destination {
     private String name;
     private Date start;
     private Date end;
     private long duration;
+    private List<User> users;
 
-    public Destination(String name, Date start, Date end, long duration) {
+    public Destination(String name, Date start, Date end, long duration, User user) {
         this.name = name;
         this.start = start;
         this.end = end;
         this.duration = duration;
+        this.users = new ArrayList<>();
+        this.users.add(user);
     }
 }
