a7cbe05016ac83a036feba06303a8083eca4a874
Aayush Anantha <aanantha9@gatech.edu>
2024-10-27 17:26:57 -0400

    modified unit tests for destination calculator

--- Project/Sprint0.5-main/app/src/test/java/com/example/sprintproject/MyStackTest.java
+++ Project/Sprint0.5-main/app/src/test/java/com/example/sprintproject/MyStackTest.java
@@ -2,6 +2,7 @@ package com.example.sprintproject;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import com.example.sprintproject.views.DestinationsActivity;
 
@@ -16,7 +17,7 @@ public class MyStackTest {
     }
 
     @Test
-    public void testCalculations() {
+    public void testDuration() {
         String startDate = "01/10/2024";
         String endDate = "09/10/2024";
         String duration = "8";
@@ -24,10 +25,27 @@ public class MyStackTest {
         long calculatedDuration = destinationsActivity.calculateDuration(startDate, endDate);
         assertEquals(8, calculatedDuration);
 
-        String calculatedEndDate = destinationsActivity.calculateEndDate(startDate,duration);
-        assertEquals(endDate,calculatedEndDate);
+    }
 
-        String calculatedStartDate = destinationsActivity.calculateStartDate(endDate, duration);
+    @Test
+    public void testStartDateCalculation() {
+        String endDate = "09/10/2024";
+        String duration = "8";
+        String startDate = "01/10/2024";
+
+        String calculatedStartDate = destinationsActivity.calculateStartDate(endDate,duration);
         assertEquals(startDate, calculatedStartDate);
     }
+
+    @Test
+    public void testEndDateCalculation() {
+        String endDate = "09/10/2024";
+        String duration = "8";
+        String startDate = "01/10/2024";
+
+        String calculatedEndDate = destinationsActivity.calculateEndDate(startDate, duration);
+        assertEquals(endDate, calculatedEndDate);
+    }
+
+
 }
